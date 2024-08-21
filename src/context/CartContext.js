8a4718397316fb989643185cfb6e478b4a07908a
Haseeb Ashraf <haseeb.ashraf@psca.gop.pk>
2024-08-21 18:54:55 +0500

    first commit

---
+++ src/context/CartContext.js
@@ -0,0 +1,27 @@
+import { createContext, useContext } from "react"
+
+const initialState = {
+    cartList: [],
+    total: 0
+
+}
+
+
+let CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+    const value = {
+        total:5000  
+    }
+    return (
+        <CartContext.Provider value={value}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+// export const useCart = () => {
+//     return useContext(CartContext); 
+
+// }
+
+    export {CartContext};
---
+++ src/pages/Cart.js
@@ -0,0 +1,31 @@
+import { useTitle } from "../hooks/useTitle";
+import { CartCard } from "../components";
+import { CartContext, useCart } from "../context/CartContext";
+import { createContext, useContext } from "react";
+
+export const Cart = () => {
+  // const {total} = useCart();
+
+  let{ total } = useContext(CartContext)
+  console.log(total)
+
+
+
+  useTitle("Cart");
+  
+  const products = [
+    {id: 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png",}, 
+    {id: 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png",}, 
+  ]
+
+  return (
+    <main>
+      <section className="cart">
+        <h1>Cart Items: {products.length} / {total}</h1>
+        { products.map((product) => (
+          <CartCard key={product.id} product={product} />
+        )) }        
+      </section>
+    </main>
+  )
+}
